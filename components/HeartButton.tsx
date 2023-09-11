9a155bcb761550f6c01450d2c1e399e5b10b06e6
Tegacreatives <contact@tegacreatives.com>
2023-09-11 23:33:13 +0100

    Created Favourite button component

---
+++ components/HeartButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+import { SafeUser } from "@/app/types";
+
+interface HeartButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  return (
+    <div
+      onClick={() => {}}
+      className="
+        relative
+        hover:opacity-80
+        transition
+        cursor-pointer
+      "
+    >
+      <AiOutlineHeart
+        size={28}
+        className="
+          fill-white
+          absolute
+          -top-[2px]
+          -right-[2px]
+        "
+      />
+      <AiFillHeart size={24} />
+    </div>
+  );
+};
+
+export default HeartButton;
