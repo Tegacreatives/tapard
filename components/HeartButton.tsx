64947efdfa462a5fa6101aab31bdcdebf45a7219
Tegacreatives <contact@tegacreatives.com>
2023-09-11 23:40:21 +0100

    Created useFavorite hook

--- components/HeartButton.tsx
+++ components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
+import useFavorite from "@/app/hooks/useFavorite";
 import { SafeUser } from "@/app/types";
 
 interface HeartButtonProps {
@@ -13,9 +14,14 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+
   return (
     <div
-      onClick={() => {}}
+      onClick={toggleFavorite}
       className="
         relative
         hover:opacity-80
@@ -32,7 +38,10 @@ const HeartButton: React.FC<HeartButtonProps> = ({
           -right-[2px]
         "
       />
-      <AiFillHeart size={24} />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
     </div>
   );
 };
