ff421a1818914740ba0dbac88863a620bb76aac2
Tegacreatives <contact@tegacreatives.com>
2023-09-17 12:16:10 +0100

    added middleware

--- components/Navbar/Search.tsx
+++ components/Navbar/Search.tsx
@@ -21,7 +21,7 @@ const Search = () => {
       return getByValue(locationValue as string)?.label;
     }
 
-    return "Anywhere";
+    return "Search anywhere...";
   }, [locationValue, getByValue]);
 
   const durationLabel = useMemo(() => {
@@ -53,14 +53,12 @@ const Search = () => {
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
     >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold pl-6 pr-20">
-          Search anything...
-        </div>
+        <div className="text-sm font-semibold pl-6 pr-20">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center text-gray-500">
-          Add Rooms
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-500 flex flex-row items-center gap-3">
-          <div className="hidden sm:block ">Add Guests</div>
+          <div className="hidden sm:block ">{guestLabel}</div>
           <div className="p-2 bg-red-500 rounded-full text-white">
             <BiSearch size={20} />
           </div>
