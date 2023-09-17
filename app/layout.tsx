9c7bc81b7a7d05098ebf50c45cc73bbebeac8486
Tegacreatives <contact@tegacreatives.com>
2023-09-17 12:08:47 +0100

    Created loading and error state

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,36 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
 
 //Local imports
-import './globals.css'
-import Navbar from '@/components/Navbar/Navbar'
-import getCurrentUser from './actions/getCurrentUser'
-import ToasterProvider from './providers/ToasterProvider'
+import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
+import getCurrentUser from "./actions/getCurrentUser";
+import ToasterProvider from "./providers/ToasterProvider";
 
 //Modals
-import RegisterModal from '@/components/Modals/RegisterModal'
-import LoginModal from '@/components/Modals/LoginModal'
-import RentModal from '@/components/Modals/RentModal'
+import RegisterModal from "@/components/Modals/RegisterModal";
+import LoginModal from "@/components/Modals/LoginModal";
+import RentModal from "@/components/Modals/RentModal";
+import SearchModal from "@/components/Modals/SearchModal";
 
-
-const inter = Nunito({ subsets: ['latin'] })
+const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Tapard',
-  description: 'Find the perfect suit for your next vacation',
-}
+  title: "Tapard",
+  description: "Find the perfect suit for your next vacation",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={inter.className}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <LoginModal />
         <RegisterModal />
@@ -37,5 +38,5 @@ export default async function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
